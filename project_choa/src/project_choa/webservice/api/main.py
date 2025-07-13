2abdc49b038886b0d365b044e3f4679409e87f64
Rodion-AI <lidofgen@gmail.com>
2025-07-13 19:16:49 +0700

    Начало работы

---
+++ project_choa/src/project_choa/webservice/api/main.py
@@ -0,0 +1,18 @@
+# import library
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# create object app FastApi
+app = FastAPI()
+
+# function '/'
+@app.get('/')
+def root():
+    return {'message': 'Hello FastAPI'}
+
+# function 'about'
+@app.get('/about')
+def about():
+    return {'message': '초아, AI for finance'}
+
+
